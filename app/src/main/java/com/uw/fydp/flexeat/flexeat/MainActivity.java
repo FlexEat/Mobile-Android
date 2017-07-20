4c20d2ccd9882cc029d2be90007bb55e7b46ba7c
Andrew Chung <andrewchungg@gmail.com>
2017-07-20 11:03:36 -0400

    add basic socket connection android

--- app/src/main/java/com/uw/fydp/flexeat/flexeat/MainActivity.java
+++ app/src/main/java/com/uw/fydp/flexeat/flexeat/MainActivity.java
@@ -3,8 +3,11 @@ package com.uw.fydp.flexeat.flexeat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,16 +16,45 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URISyntaxException;
 import java.net.URL;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import com.github.nkzawa.emitter.Emitter;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://192.168.0.21:3000/");
+            Log.i("INFO","SOCKET PASS");
+        } catch (Exception e) {
+            Log.i("INFO", "SOCKET FAIL");
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSocket.disconnect();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSocket.on("message", onNewMessage);
+        try {
+            mSocket.connect();
+            Log.i("INFO","SOCKET PASS CONNECT");
+        }
+        catch (Exception e) {
+            Log.i("INFO", "SOCKET CONNECT FAIL");
+        }
         setContentView(R.layout.activity_main);
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         GraphRequest request = GraphRequest.newMeRequest(accessToken,
@@ -50,9 +82,38 @@ public class MainActivity extends AppCompatActivity {
         checkInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("INFO","ON CLICK!");
+                String message = "test message";
+                mSocket.emit("message", message);
+
                 Intent intent = new Intent(MainActivity.this, CheckInActivity.class);
-                startActivity(intent);
+//                startActivity(intent);
             }
         });
     }
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+//                    String username;
+//                    String message;
+//                    try {
+//                        username = data.getString("username");
+//                        message = data.getString("message");
+//                    } catch (JSONException e) {
+//                        return;
+//                    }
+//
+//                    // add the message to view
+//                    addMessage(username, message);
+                    Log.i("INFO", "MESSAGE RECEIVED FROM SOCKET");
+                    Log.i("INFO", data.toString());
+                }
+            });
+        }
+    };
 }

814a683d48d4da5348907af3233b28776a87f398
Chaitanya Khanna <c2khanna@uwaterloo.ca>
2017-12-14 01:19:26 -0500

    Revert "hamburger menu layout ready"
    This reverts commit f9ac9e3301fc39740f17eacb9af35d364a534637.

--- app/src/main/java/com/uw/fydp/flexeat/flexeat/CheckInActivity.java
+++ app/src/main/java/com/uw/fydp/flexeat/flexeat/CheckInActivity.java
@@ -1,20 +1,13 @@
 package com.uw.fydp.flexeat.flexeat;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class CheckInActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+public class CheckInActivity extends AppCompatActivity {
 
     EditText restaurantCode;
     @Override
@@ -22,17 +15,6 @@ public class CheckInActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
         restaurantCode = (EditText) findViewById(R.id.check_in_code_field);
     }
 
@@ -44,40 +26,5 @@ public class CheckInActivity extends AppCompatActivity
         else{
             Toast.makeText(getApplicationContext(), "Wrong code", Toast.LENGTH_LONG).show();
         }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
--- app/src/main/java/com/uw/fydp/flexeat/flexeat/MenuActivity.java
+++ app/src/main/java/com/uw/fydp/flexeat/flexeat/MenuActivity.java
@@ -1,11 +1,7 @@
 package com.uw.fydp.flexeat.flexeat;
 
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -22,8 +18,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MenuActivity extends AppCompatActivity
-        implements MenuItemInterface, NavigationView.OnNavigationItemSelectedListener {
+public class MenuActivity extends AppCompatActivity implements MenuItemInterface {
 
     ArrayList<MenuItem> selectedItems = new ArrayList<>();
     JSONObject menuResponse = new JSONObject();
@@ -46,15 +41,6 @@ public class MenuActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
 
         JSONArray appetizerJSONArray = new JSONArray();
@@ -148,40 +134,5 @@ public class MenuActivity extends AppCompatActivity
                 e.printStackTrace();
             }
         }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(android.view.MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
